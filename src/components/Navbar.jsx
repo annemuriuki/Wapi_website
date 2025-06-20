b2ee4778a28efc5882d395cfdaf6da9cdb375123
Anne Muriuki <muriukianne05@gmail.com>
2025-06-20 15:04:06 +0300

    added buttons on mobile

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,18 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import "./Navbar.css";
+import './Navbar.css';
 import successrate from '../assets/success-rate.png';
-import { FaBars, FaTimes } from 'react-icons/fa'; 
+import { FaBars, FaTimes } from 'react-icons/fa';
 
 const Navbar = () => {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
-  
+
   const toggleMobileMenu = () => {
     setMobileMenuOpen(!mobileMenuOpen);
+  };
+
+  const closeMobileMenu = () => {
+    setMobileMenuOpen(false);
   };
 
   return (
@@ -19,29 +23,32 @@ const Navbar = () => {
           <span style={{ color: '#103713' }}>Transact</span>Flow
         </Link>
       </div>
-      
+
       <button className="mobile-menu-button" onClick={toggleMobileMenu}>
         {mobileMenuOpen ? <FaTimes size={24} /> : <FaBars size={24} />}
       </button>
-      
+
       <div className={`navbar-links-container ${mobileMenuOpen ? 'active' : ''}`}>
         <div className="navbar-links">
-          <Link to="/" className="navbar-link" onClick={() => setMobileMenuOpen(false)}>Home</Link>
-          <Link to="/about" className="navbar-link" onClick={() => setMobileMenuOpen(false)}>About</Link>
-          <Link to="/solutions" className="navbar-link" onClick={() => setMobileMenuOpen(false)}>Solutions</Link>
-          <Link to="/blog" className="navbar-link" onClick={() => setMobileMenuOpen(false)}>Blog</Link>
-          <Link to="/faq" className="navbar-link" onClick={() => setMobileMenuOpen(false)}>FAQ's</Link>
-          <Link to="/contact" className="navbar-link" onClick={() => setMobileMenuOpen(false)}>Contact</Link>
+          <Link to="/" className="navbar-link" onClick={closeMobileMenu}>Home</Link>
+          <Link to="/about" className="navbar-link" onClick={closeMobileMenu}>About</Link>
+          <Link to="/solutions" className="navbar-link" onClick={closeMobileMenu}>Solutions</Link>
+          <Link to="/blog" className="navbar-link" onClick={closeMobileMenu}>Blog</Link>
+          <Link to="/faq" className="navbar-link" onClick={closeMobileMenu}>FAQ's</Link>
+          <Link to="/contact" className="navbar-link" onClick={closeMobileMenu}>Contact</Link>
+        </div>
+
+        {/* Mobile-only auth buttons */}
+        <div className="navbar-right mobile-only">
+          <Link to="/signup" className="navbar-sign-up" onClick={closeMobileMenu}>Sign Up</Link>
+          <Link to="/signin" className="navbar-sign-in" onClick={closeMobileMenu}>Sign In</Link>
         </div>
       </div>
-      
-      <div className={`navbar-right ${mobileMenuOpen ? 'active' : ''}`}>
-        <Link to="/signup" className="navbar-sign-up" onClick={() => setMobileMenuOpen(false)}>
-          Sign Up
-        </Link>
-        <Link to="/signin" className="navbar-sign-in" onClick={() => setMobileMenuOpen(false)}>
-          Sign in
-        </Link>
+
+      {/* Desktop-only auth buttons */}
+      <div className="navbar-right desktop-only">
+        <Link to="/signup" className="navbar-sign-up">Sign Up</Link>
+        <Link to="/signin" className="navbar-sign-in">Sign In</Link>
       </div>
     </nav>
   );
